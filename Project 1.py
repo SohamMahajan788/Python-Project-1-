4b35a7307e7d2c1fe25c226899b1e15cd8f149ef
SohamMahajan788 <sohammahajan788@gmail.com>
2025-02-17 21:10:31 +0530

    first commit

---
+++ Project 1.py
@@ -0,0 +1,4 @@
+print("Hello XYZ, lets find a Band name for you")
+city = input("What is your city's name: \n ")
+pet_name = input("What is your pet's name: \n ")
+print("Here is your awesome Band name: " + city + " " + pet_name)
